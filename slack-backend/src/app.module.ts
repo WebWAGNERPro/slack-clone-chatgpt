4968a9a8084126ce28ccea0bf6e0971c2a24e0e9
WebWAGNERPro <contact@webwagner.fr>
2025-04-13 18:02:19 +0200

    feat: add modules for authentication, users, chat, tasks, and calendar

--- slack-backend/src/app.module.ts
+++ slack-backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ChatModule } from './chat/chat.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CalendarModule } from './calendar/calendar.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ChatModule, TasksModule, CalendarModule],
   controllers: [AppController],
   providers: [AppService],
 })
